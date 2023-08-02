0cd7bc5e76a8aa4a5e9cfb93d0c06987fca58a6c
arlysanchez <arly.sanchez@upeu.edu.pe>
2023-08-01 22:49:06 -0500

    Mostrar información del producto en la pagina del usuario

--- src/main/java/upeu/edu/pe/ecommerce/controllers/HomeController.java
+++ src/main/java/upeu/edu/pe/ecommerce/controllers/HomeController.java
@@ -5,12 +5,15 @@
 package upeu.edu.pe.ecommerce.controllers;
 
 import jakarta.servlet.http.HttpSession;
+import java.util.Optional;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import upeu.edu.pe.ecommerce.models.Producto;
 import upeu.edu.pe.ecommerce.services.ProductoService;
 
 /**
@@ -38,5 +41,16 @@ public class HomeController {
 
         return "usuario/home";
     }
+    @GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("Id producto enviado como parámetro {}", id);
+		      Producto producto = new Producto();
+		      Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+
+		model.addAttribute("producto", producto);
+
+		return "usuario/productohome";
+	}
 
 }
